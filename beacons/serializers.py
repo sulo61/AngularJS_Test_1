1e478acdb8a761100cd2ba5f41f3fd8034acc17b
Mateusz Maciolek <mateusz.maciolek@coders-mill.com>
2015-09-29 15:50:13 +0200

    Validation create shops

--- beacons/serializers.py
+++ beacons/serializers.py
@@ -36,19 +36,37 @@ class ShopSerializer(serializers.HyperlinkedModelSerializer):
             OpeningHours.objects.create(shop=shop, **opening_hours)
         return shop
 
-    # def is_valid(self, raise_exception=False):
-    #     valid = super(ShopSerializer, self).is_valid(raise_exception)
-    #     # opening_hours = self._data.pop('opening_hours')
-    #     # self.valid_days(opening_hours)
-    #
-    #     return valid
+    def is_valid(self, raise_exception=False):
+        valid = super(ShopSerializer, self).is_valid(raise_exception)
+        opening_hours = self.data.get('opening_hours')
+        if valid:
+            valid = self.valid_days(opening_hours)
+
+        return valid
 
     def valid_days(self, opening_hours):
         if not opening_hours:
             return False
+
+        hours_ = opening_hours[0]
+        if hours_.get('days')[0] != 1:
+            raise ValidationError(detail={'open_hours': ['Days should starts with 0']})
+
+        hours_last = opening_hours[-1]
+        if hours_last.get('days')[-1] != 7:
+            raise ValidationError(detail={'open_hours': ['Days should ends with 7']})
+
+        day_before = None
         for opening_hour in opening_hours:
             if not opening_hour:
                 return False
                 # raise ValidationError("opening_hour can not be empty")
-            for day in opening_hour.days[1:len(opening_hour)]:
-                pass
+            for day in opening_hour.get('days'):
+                if day_before is None:
+                    day_before = day
+                    continue
+                if day != day_before + 1:
+                    raise ValidationError(detail={'open_hours': ['Days should by ordered constantly form 1 to 7']})
+
+                day_before = day
+        return True
--- tests.py
+++ tests.py
@@ -3,11 +3,14 @@ from django.test import TestCase
 # Create your tests here.
 from beacons.serializers import ShopSerializer
 
-opening_days = {
-    
+test_json = {
+    "name": "Hello",
+    "opening_hours": [{"days": [1, 2, 3], "open_time": "20:00:00.000000", "close_time": "20:00:00.000000"},
+                      {"days": [4, 5, 6, 7], "open_time": "20:00:00.000000", "close_time": "20:00:00.000000"}]
 }
+
+
 class ValidationTest(TestCase):
     def test_validation(self):
         serializer = ShopSerializer()
-        days = {'days': [1, 2, 3, 4], 'days': [5, 6, 7]}
-        self.assertTrue(serializer.valid_days(days), "Should be true")
+        self.assertTrue(serializer.valid_days(test_json.pop('opening_hours')), "Should be true")
