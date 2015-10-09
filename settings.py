3c1a876468d6e8e3272b3f3729e016c458e53196
MateuszM <mateuszx90@gmail.com>
2015-10-09 15:44:03 +0200

    migrations

--- settings.py
+++ settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = 'images'
 
 STATIC_URL = '/images/'
 
