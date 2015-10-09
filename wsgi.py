179df04928012ebc203d6c29c05fb78967d1a312
MateuszM <mateuszx90@gmail.com>
2015-10-09 16:34:51 +0200

    Static files

--- wsgi.py
+++ wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
