20d55766ed32b88d4bdaea65bef1cb82f1386863
Mateusz Maciolek <mateusz.maciolek@coders-mill.com>
2015-10-15 16:49:03 +0200

    Fixed settings

--- settings.py
+++ settings.py
@@ -113,13 +113,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = 'staticfiles'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 # STATIC_URL = '/static/'
 #
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 REST_FRAMEWORK = {
@@ -162,3 +162,31 @@ except:
         from heroku_settings import *
     except:
         pass
+
+AWS_STORAGE_BUCKET_NAME = 'beacons-project'
+AWS_ACCESS_KEY_ID = 'AKIAJEMTKLDCTAFSKB7A'
+AWS_SECRET_ACCESS_KEY = '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'
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+# STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+# STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
+                 'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+                 'Cache-Control': 'max-age=94608000',
+                 }
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
