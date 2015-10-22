a9f3883fd6b22a8df67e3f0efe5a94752cb4e410
Mateusz Maciolek <mateusz.maciolek@coders-mill.com>
2015-10-22 19:26:11 +0200

    Integrated tests

--- tests.py
+++ tests.py
@@ -1,7 +1,13 @@
+import json
 from django.test import TestCase
 
 # Create your tests here.
 from beacons.serializers import ShopSerializer
+from rest_framework import status
+from rest_auth.tests import APIClient
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
 
 test_json = {
     "name": "Hello",
@@ -11,6 +17,67 @@ test_json = {
 
 
 class ValidationTest(TestCase):
-    def test_validation(self):
+    def test3_validation(self):
         serializer = ShopSerializer()
         self.assertTrue(serializer.valid_days(test_json.pop('opening_hours')), "Should be true")
+
+
+class UserRegisterCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_register(self):
+        register_data = {
+            'email': 'mcol@gmail.com',
+            'password': 'mcol',
+        }
+
+        response = self.client.post('/register/', register_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        loads = json.loads(response.content)
+        self.assertEqual(loads, {
+            u"id": loads.get('id'),
+            u"email": u"mcol@gmail.com",
+            u"first_name": u'',
+            u"last_name": u'',
+            u"address": u''
+        })
+
+
+def register(client):
+    register_data = {
+        'email': 'mcol@gmail.com',
+        'password': 'mcol',
+    }
+    response = client.post('/register/', register_data, format='json')
+    return register_data, response
+
+
+class UserLoginCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_register(self):
+        register_data, response = register(self.client)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.post('/login/', register_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        loads = json.loads(response.content)
+        self.assertTrue('token' in loads)
+
+
+
+
+class UserPermissionsCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_register(self):
+        response = self.client.get('/campaigns/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user(self):
+        register_data, response = register(self.client)
+        response = self.client.get('/user/27/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
