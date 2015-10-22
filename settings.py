7dc4e1becd892e83554c650b1b228fd4abba5d5c
Mateusz Maciolek <mateusz.maciolek@coders-mill.com>
2015-10-22 15:45:47 +0200

    Removed cors

--- settings.py
+++ settings.py
@@ -60,7 +60,6 @@ INSTALLED_APPS = (
     'imagekit',
     'gunicorn',
     'storages',
-    'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -72,8 +71,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
