7f91f2cccf60ea76f4d9787f045ff99530479a28
sulo61 <sulo612@gmail.com>
2015-10-28 14:49:27 +0100

    shop style problem

--- static/templates/Panel/Shop/shopController.js
+++ static/templates/Panel/Shop/shopController.js
@@ -1,6 +1,22 @@
 angular.module('panelApp').controller('shopController', ['$scope', '$http', '$routeParams', 'apiInfo', function($scope, $http, $routeParams, apiInfo){
 	// api info
 	this.apiInfo = apiInfo;
+	// shop id
 	this.id = $routeParams.id;
+	// model
+	this.shop = {};
+	// get shop
+	this.getShop = function(id){
+		$http({
+			method: 'GET',
+			url: '/shops/'+id+"/"
+		}).then(function successCallback(response){
+			this.shop = response.data;
+		}.bind(this), function errorCallback(response){
+			apiInfo.showFail(response);
+		}.bind(this));	
+	}
+
+	this.getShop(this.id);
 	
 }]);
