f16fe3d868970c501727c71d51317bc691872267
Mateusz Maciolek <mateusz.maciolek@coders-mill.com>
2015-10-29 17:56:31 +0100

    Test image in html

--- settings.py
+++ settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 #
 STATICFILES_DIRS = (
