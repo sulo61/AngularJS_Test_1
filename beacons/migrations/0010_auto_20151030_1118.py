0a5426d8b915624390cc1e4a458788aa4cb28158
Mateusz Maciolek <mateusz.maciolek@coders-mill.com>
2015-10-30 11:42:17 +0100

    Removed all records UserDetails

--- beacons/migrations/0010_auto_20151030_1118.py
+++ beacons/migrations/0010_auto_20151030_1118.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations
+
+
+def remove_award_detail(apps, chema_editor):
+    apps.get_model('beacons', 'UserAwards').objects.all().delete()
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('beacons', '0009_userawards'),
     ]
 
     operations = [
+        migrations.RunPython(remove_award_detail),
         migrations.AlterUniqueTogether(
             name='userawards',
             unique_together=set([('award', 'user')]),
