bfa2e73f238f0c01f557eb26ac4ba1004ada7935
sulo61 <sulo612@gmail.com>
2015-11-03 14:33:54 +0100

    add ad list

--- static/templates/Panel/Campaign/Ads/adsController.js
+++ static/templates/Panel/Campaign/Ads/adsController.js
@@ -1,10 +1,61 @@
 angular.module('panelApp').controller('adsController', ['$scope', '$http', '$routeParams', 'appInfo', function($scope, $http, $routeParams, appInfo){
-// api info
+	// api info
 	this.appInfo = appInfo;
 	// campaign params
 	this.id = $routeParams.id;
 	this.name = $routeParams.name;
-	// model
-	
+	// models
+	this.adsList = [];
+	this.adsPages = [];	// numbers
+	this.adsCurrentPage = 1;
+	// nav
+	this.adsNavActive = function(page){
+		if (page==this.adsCurrentPage){
+			return "active"
+		} else {
+			return "";
+		}
+	};
+	this.adsNavNext = function(){
+		if (this.adsCurrentPage<this.adsPages.length){
+			this.getAds(this.adsCurrentPage+1);
+		}
+	};
+	this.adsNavPrev = function(){
+		if (this.adsCurrentPage>1){
+			this.getAds(this.adsCurrentPage-1);	
+		}
+	};
+	// api
+	this.getAds = function(page){
+		$http({
+			method: 'GET',
+			url: '/campaigns/'+this.id+'/ads',
+			params: {"page" : page}
+		}).then(function successCallback(response){
+			this.adsList = [];
+			this.adsPages = [];
+			this.adsList = response.data.results;
+			for (var i=0; i<Math.ceil((response.data.count/5)); i++) {
+		    	this.adsPages.push(i+1);
+		    }
+		    this.adsCurrentPage = page;
+		}.bind(this), function errorCallback(response){
+			appInfo.showFail(response);
+		});	
+	};
+	this.deleteAward = function(adID, index){
+		$http({
+			method: 'DELETE',
+			url: '/campaigns/'+this.id+'/ads/'+adID
+		}).then(function successCallback(response){
+			appInfo.showSuccess();
+			this.getAds(this.adsCurrentPage);
+		}.bind(this), function errorCallback(response){
+			appInfo.showFail(response);
+		});	
+	}
+
+	this.getAds(1);
 
 }]);
