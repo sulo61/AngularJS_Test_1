5fd482a87ab2c2e4674968541198bc5caed235ef
sulo61 <sulo612@gmail.com>
2015-11-09 14:10:15 +0100

    add actions list

--- static/templates/Panel/Campaign/Actions/actionsController.js
+++ static/templates/Panel/Campaign/Actions/actionsController.js
@@ -1,33 +1,66 @@
 angular.module('panelApp').controller('actionsController', ['$scope', '$http', '$routeParams', 'appInfo', function($scope, $http, $routeParams, appInfo){
-// api info
+	// api info
 	this.appInfo = appInfo;
 	// campaign params
-	this.id = $routeParams.id;
-	// model
-	this.model = 
-	{
-	  "beacon": "",
-	  "ad": {
-	    "id": 0,
-	    "title": "",
-	    "description": "",
-	    "image": "",
-	    "type": "choice"
-	  },
-	  "points": 0
-	}
-
-	this.lol = function(){
+	this.campaignID = $routeParams.id;
+	// models
+	this.actionsList = [];
+	this.actionsPages = [];	// numbers
+	this.actionsCurrentPage = 1;
+	this.numberOfItems = 0;
+	// nav
+	this.actionsNavActive = function(page){
+		if (page==this.actionsCurrentPage){
+			return "active"
+		} else {
+			return "";
+		}
+	};
+	this.actionsNavNext = function(){
+		if (this.actionsCurrentPage<this.actionsPages.length){
+			this.getActions(this.actionsCurrentPage+1);
+		}
+	};
+	this.actionsNavPrev = function(){
+		if (this.actionsCurrentPage>1){
+			this.getActions(this.actionsCurrentPage-1);	
+		}
+	};
+	// api
+	this.getActions = function(page){
 		$http({
-			method: 'POST',
-			url: '/api/campaigns/'+this.campaignID+'/actions/',
-			data: this.model
-		}).then(function successCallback(response){			
-			appInfo.showSuccess();			
+			method: 'GET',
+			url: '/api/campaigns/'+this.campaignID+'/actions',
+			params: {"page" : page}
+		}).then(function successCallback(response){
+			this.actionsList = [];
+			this.actionsPages = [];
+			this.actionsList = response.data.results;
+			this.numberOfItems = response.data.count;
+			for (var i=0; i<Math.ceil((this.numberOfItems/5)); i++) {
+		    	this.actionsPages.push(i+1);
+		    }
+		    this.actionsCurrentPage = page;
 		}.bind(this), function errorCallback(response){
 			appInfo.showFail(response);
-		}.bind(this));
+		});	
+	};
+	this.deleteAction = function(actionID, index){
+		$http({
+			method: 'DELETE',
+			url: '/api/campaigns/'+this.campaignID+'/actions/'+actionID
+		}).then(function successCallback(response){
+			appInfo.showSuccess();
+			this.numberOfItems = this.numberOfItems - 1;
+			if ( (this.numberOfItems <= (this.actionsCurrentPage-1) * 5) && this.numberOfItems>=5 ){
+				this.actionsCurrentPage = this.actionsCurrentPage - 1;
+			}
+			this.getActions(this.actionsCurrentPage);
+		}.bind(this), function errorCallback(response){
+			appInfo.showFail(response);
+		});	
 	}
-	
+
+	this.getActions(1);
 
 }]);
